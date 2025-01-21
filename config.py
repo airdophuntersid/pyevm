844a0e84739208e6a94a400b381f25510f36fd9f
airdrophuntersid <airdrops.hunters.id@gmail.com>
2025-01-21 19:45:16 +0700

    .

---
+++ config.py
@@ -0,0 +1,21 @@
+# pyevm < https://t.me/airdrophuntersid >
+# Copyright (C) 2025-present @airdrophuntersid
+#
+# This file is a part of < https://github.com/airdrophuntersid/pyevm/ >
+# Please read the GNU General Public License v3.0
+# < https://github.com/airdophuntersid/pyevm/blob/main/LICENSE >
+
+from os import getenv as gets
+from dotenv import find_dotenv, load_dotenv
+from typing import Union
+from sys import exit
+
+load_dotenv(
+    find_dotenv(
+        "config.env"
+    )
+)
+
+MAX_SEND: Union[int, float] = gets("MAX_SEND", "")
+MIN_SEND: Union[int, float] = gets("MIN_SEND", "")
+PRIVATE_KEY: Union[str] = gets("PRIVATE_KEY", "").split(",")
---
+++ run.py
@@ -0,0 +1,13 @@
+# pyevm < https://t.me/airdrophuntersid >
+# Copyright (C) 2025-present @airdrophuntersid
+#
+# This file is a part of < https://github.com/airdrophuntersid/pyevm/ >
+# Please read the GNU General Public License v3.0
+# < https://github.com/airdophuntersid/pyevm/blob/main/LICENSE >
+import os
+os.system("clear")
+from config import MAX_SEND, MIN_SEND, PRIVATE_KEY
+
+print(MAX_SEND)
+print(MIN_SEND)
+print(PRIVATE_KEY)
